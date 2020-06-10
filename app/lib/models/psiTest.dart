2ba962d5e509dba040c1a3b04e7320feb6f2efa6
Kris Randall <kris@cocreations.com.au>
2020-06-10 18:46:59 +1000

    An earlier change moving the PsiTest class forward,  BUT, prior to replacing it with the BLoC pattern.

--- app/lib/models/psiTest.dart
+++ app/lib/models/psiTest.dart
@@ -4,13 +4,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 enum PsiTestStatus {
   HAVE_NOT_CHECKED_SERVER_FOR_EXISTING_YET,
-  HAVE_MULTIPLE_TESTS_ON_SERVER,
-  NOT_STARTED,
+  ERROR_STATE,
+  NO_ACTIVE_TEST,
   AWAITING_SENDER,
   AWAITING_RECEIVER,
   UNDERWAY,
   COMPLETED,
   CANCELLED,
+}
+
+enum PsiTestError {
+  HAVE_MULTIPLE_TESTS_ON_SERVER,
+
 }
 
 enum PsiTestRole {
@@ -25,6 +30,7 @@ class PsiTest {
   String testId;
   PsiTestStatus testStatus;
   PsiTestRole myRole;
+  PsiTestError error;
   int totalNumQuestions;
   int numQuestionsAnswered;
   PsiTestQuestion currentQuestion;
@@ -35,13 +41,36 @@ class PsiTest {
 
   PsiTest() {
     testId = "not_currently_used";
-    testStatus = PsiTestStatus.HAVE_NOT_CHECKED_SERVER_FOR_EXISTING_YET; // (myRole==PsiTestRole.SENDER) ? PsiTestStatus.AWAITING_RECEIVER : PsiTestStatus.AWAITING_SENDER;
+    testStatus = PsiTestStatus.HAVE_NOT_CHECKED_SERVER_FOR_EXISTING_YET;
     totalNumQuestions = DEFAULT_NUM_QUESTIONS;
     numQuestionsAnswered = 0;
   }
 
   void beginNewTestAsSender() {
+    if (testStatus!=PsiTestStatus.NO_ACTIVE_TEST) throw "Cannot begin new test unless current status is NO_ACTIVE_TEST";
     myRole = PsiTestRole.SENDER;
+    testStatus = PsiTestStatus.AWAITING_RECEIVER;
+  }
+
+  void beginNewTestAsReceiver() {
+    if (testStatus!=PsiTestStatus.NO_ACTIVE_TEST) throw "Cannot begin new test unless current status is NO_ACTIVE_TEST";
+    myRole = PsiTestRole.RECEIVER;
+    testStatus = PsiTestStatus.AWAITING_SENDER;
+  }
+
+  void createNewTestOnServer() async {
+    if (testStatus!=PsiTestStatus.AWAITING_RECEIVER && testStatus!=PsiTestStatus.AWAITING_SENDER) {
+      throw "Can't save new test unless status is AWAITING_RECEIVER or AWAITING_SENDER";
+    }
+
+  DocumentReference ref = await _databaseReference.collection("test")
+    .add({
+      'title': 'Flutter in Action',
+      'description': 'Complete Programming Guide to learn Flutter'
+    });
+
+    testId = ref.documentID;
+
   }
 
 
@@ -53,13 +82,14 @@ class PsiTest {
       .then((QuerySnapshot snapshot) {
 
         if (snapshot.documents.length>1) {
-          testStatus = PsiTestStatus.HAVE_MULTIPLE_TESTS_ON_SERVER;
+          testStatus = PsiTestStatus.ERROR_STATE;
+          error = PsiTestError.HAVE_MULTIPLE_TESTS_ON_SERVER;
         } else if (snapshot.documents.length==0) {
-          testStatus = PsiTestStatus.NOT_STARTED;
+          testStatus = PsiTestStatus.NO_ACTIVE_TEST;
         } else {
 
           // TODO -- add in all the database data 
-          //snapshot.documents.forEach((f) => print('${f.data}}'));
+          snapshot.documents.forEach((f) => print('${f.data}}'));
 
         }
 
