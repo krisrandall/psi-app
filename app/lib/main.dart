11fc260583b832fb85dd121fb9f349307798ca12
rabotnik1 <randall@posteo.de>
2020-08-17 14:18:19 +0200

    not working, but code written

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:wasm';
 
 import 'package:app/bloc/psitestsave_bloc.dart';
 import 'package:app/components/livePsiTestStream.dart';
@@ -10,36 +11,35 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:flutter/services.dart' show PlatformException;
+import 'dart:async';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     /* to prevent device rotation - but not proven yet if works, or needed... 
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
     */
-    
+
     return BlocProvider(
-      create: (context) =>
-          PsiTestSaveBloc(),
+      create: (context) => PsiTestSaveBloc(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.indigo,
           scaffoldBackgroundColor: Color.fromRGBO(47, 11, 56, 1),
         ),
-        home:  LandingPage(),
+        home: LandingPage(),
       ),
     );
   }
 }
-
-
 
 class LandingPage extends StatefulWidget {
   @override
@@ -47,10 +47,9 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
-
   String signinErrorMessage = "";
 
-  void initDynamicLinks() async {
+  /*void initDynamicLinks() async {
 
     print('in initDynamicLinks()');
 
@@ -64,32 +63,70 @@ class _LandingPageState extends State<LandingPage> {
     FirebaseDynamicLinks.instance.onLink(
       onSuccess: (PendingDynamicLinkData dynamicLink) async {
         final Uri deepLink = dynamicLink?.link;
+*/
+  Future<Null> initUniLinks() async {
+    try {
+      StreamSubscription _sub;
+      _sub = getUriLinksStream().listen((Uri deepLink) {
+        print("listener for getUriLinksStream() returned a link: $deepLink");
+        return deepLink;
+        //this never gets called
+        goToScreen(
+            context,
+            OpenedViaLinkWidget(deepLink,
+                'listener for getUriLinksStream() returned the link'));
+      }, onError: (err) {
+        print("error with Stream getting deepLink $err");
+      });
 
-        if (deepLink != null) {
-          goToScreen(context, OpenedViaLinkWidget(deepLink));
-        }
-      },
+      void dispose(filename) {
+        _sub.cancel();
+      }
+
+      Uri deepLink = await getInitialUri().then((deepLink) {
+        print(
+            "Future<Uri> Object was provided by getInitialUri(), and the link is: $deepLink");
+        return deepLink;
+
+        /* goToScreen(
+            context,
+            OpenedViaLinkWidget(
+                deepLink, 'Future Uri Object provided by getInitialUri()'));*/
+      });
+      //this never gets called
+      if (deepLink != null) {
+        goToScreen(context, OpenedViaLinkWidget(deepLink, 'working'));
+      }
+    } on FormatException {
+      print("couldn't parse the Uri $FormatException");
+    } on PlatformException {
+      print("couldn't retrieve initital link $PlatformException");
+    }
+  }
+
+// ...
+
+  /* },
       onError: (OnLinkErrorException e) async {
         print('onLinkError');
         print(e.message);
       }
-    );
-  }
+    )
+  }}*/
 
   @override
   Widget build(BuildContext context) {
-
     Future<void> _signInAnonymously() async {
       try {
         await precacheImage(AssetImage('assets/table.jpg'), context);
         await precacheImage(AssetImage('assets/splash.png'), context);
         await FirebaseAuth.instance.signInAnonymously();
-        initDynamicLinks(); 
+        initUniLinks();
       } catch (e) {
-        setState(() { 
-          signinErrorMessage = "Unable to Sign in\n"+
-            "Check your internet connection\n" +
-            e.toString();
+        setState(() {
+          signinErrorMessage = "Unable to Sign in\n" +
+              "Check your internet connection\n" +
+              e.toString();
         });
       }
     }
@@ -100,37 +137,33 @@ class _LandingPageState extends State<LandingPage> {
       appBar: AppBar(
         title: Text('𝚿 Psi Telepathy Test'),
       ),
-      body: TableBgWrapper(
-        StreamBuilder<FirebaseUser>(
-          stream: FirebaseAuth.instance.onAuthStateChanged,
-          builder: (context, snapshot) {
-            print(snapshot.connectionState);
-            if (snapshot.connectionState == ConnectionState.active) {
-              
-              FirebaseUser user = snapshot.data;
-              if (user == null) {
-                return Column(children: <Widget>[
-                      CircularProgressIndicator(),
-                      Text('Logging in ..'),
-                    ]
-                  );
-              } else if (signinErrorMessage!='') {
-                return TitleText( signinErrorMessage );
-              } else {
-                globalCurrentUser = user;
-                return AfterAuthWidget();
-              }
-              
-            } else {
+      body: TableBgWrapper(StreamBuilder<FirebaseUser>(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (context, snapshot) {
+          print(snapshot.connectionState);
+          if (snapshot.connectionState == ConnectionState.active) {
+            FirebaseUser user = snapshot.data;
+            if (user == null) {
               return Column(children: <Widget>[
-                      CircularProgressIndicator(),
-                      Text('Connecting ..'),
-                    ],
-              );
+                CircularProgressIndicator(),
+                Text('Logging in ..'),
+              ]);
+            } else if (signinErrorMessage != '') {
+              return TitleText(signinErrorMessage);
+            } else {
+              globalCurrentUser = user;
+              return AfterAuthWidget();
             }
-          },
-        )
-      ),
+          } else {
+            return Column(
+              children: <Widget>[
+                CircularProgressIndicator(),
+                Text('Connecting ..'),
+              ],
+            );
+          }
+        },
+      )),
     );
   }
 }
@@ -139,30 +172,32 @@ class AfterAuthWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: firestoreDatabaseStream.snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (psiTestNotAvailable(snapshot)) return psiTestNotAvailableWidget(snapshot);
-        var currentTest = createTestFromFirestore(snapshot.data.documents);
-        return HomePage(currentTest);
-      }
-    );
+        stream: firestoreDatabaseStream.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (psiTestNotAvailable(snapshot))
+            return psiTestNotAvailableWidget(snapshot);
+          var currentTest = createTestFromFirestore(snapshot.data.documents);
+          return HomePage(currentTest);
+        });
   }
 }
 
 class OpenedViaLinkWidget extends StatelessWidget {
-
   final Uri deepLink;
-  OpenedViaLinkWidget(this.deepLink);
+  final String deepLinkOrigin;
+  OpenedViaLinkWidget(this.deepLink, this.deepLinkOrigin);
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: firestoreDatabaseStream.snapshots(), // TO CHANNGE TO QUERY BASED ON INPUT PARAM
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (psiTestNotAvailable(snapshot)) return psiTestNotAvailableWidget(snapshot);
-        var currentTest = createTestFromFirestore(snapshot.data.documents);
-        return CopyText('Screen for joining a test invitation ... ${deepLink.toString()} ... ');
-      }
-    );
+        stream: firestoreDatabaseStream
+            .snapshots(), // TO CHANNGE TO QUERY BASED ON INPUT PARAM
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (psiTestNotAvailable(snapshot))
+            return psiTestNotAvailableWidget(snapshot);
+          var currentTest = createTestFromFirestore(snapshot.data.documents);
+          return CopyText(
+              'Screen for joining a test invitation ... ${deepLink.toString()} ...link from $deepLinkOrigin ');
+        });
   }
 }
